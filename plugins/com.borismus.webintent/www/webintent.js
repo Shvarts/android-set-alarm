b5ef629bc4406d1d11b965dc02795440f3b0dca0
Shvarts Oleh <oleh.shvarts.sp@lhsystems.com>
2016-09-09 10:54:36 +0300

    updated plugin sources

--- plugins/com.borismus.webintent/www/webintent.js
+++ plugins/com.borismus.webintent/www/webintent.js
@@ -1,3 +1,4 @@
+cordova.define("com.borismus.webintent.WebIntent", function(require, exports, module) {
 /**
  * cordova Web Intent plugin
  * Copyright (c) Boris Smus 2010
@@ -17,57 +18,19 @@
     WebIntent.prototype.ACTION_CALL = "android.intent.action.CALL";
     WebIntent.prototype.ACTION_SENDTO = "android.intent.action.SENDTO";
 
-    WebIntent.prototype.startActivity = function(params, success, fail) {
+    WebIntent.prototype.setAlarm = function(params, success, fail) {
         return cordova.exec(function(args) {
             success(args);
         }, function(args) {
             fail(args);
-        }, 'WebIntent', 'startActivity', [params]);
-    };
-
-    WebIntent.prototype.hasExtra = function(params, success, fail) {
-        return cordova.exec(function(args) {
-            success(args);
-        }, function(args) {
-            fail(args);
-        }, 'WebIntent', 'hasExtra', [params]);
-    };
-
-    WebIntent.prototype.getUri = function(success, fail) {
-        return cordova.exec(function(args) {
-            success(args);
-        }, function(args) {
-            fail(args);
-        }, 'WebIntent', 'getUri', []);
-    };
-
-    WebIntent.prototype.getExtra = function(params, success, fail) {
-        return cordova.exec(function(args) {
-            success(args);
-        }, function(args) {
-            fail(args);
-        }, 'WebIntent', 'getExtra', [params]);
-    };
-
-
-    WebIntent.prototype.onNewIntent = function(callback) {
-        return cordova.exec(function(args) {
-            callback(args);
-        }, function(args) {
-        }, 'WebIntent', 'onNewIntent', []);
-    };
-
-    WebIntent.prototype.sendBroadcast = function(params, success, fail) {
-        return cordova.exec(function(args) {
-            success(args);
-        }, function(args) {
-            fail(args);
-        }, 'WebIntent', 'sendBroadcast', [params]);
+        }, 'WebIntent', 'setAlarm', [params]);
     };
 
     window.webintent = new WebIntent();
-    
+
     // backwards compatibility
     window.plugins = window.plugins || {};
     window.plugins.webintent = window.webintent;
 })(window.PhoneGap || window.Cordova || window.cordova);
+
+});
